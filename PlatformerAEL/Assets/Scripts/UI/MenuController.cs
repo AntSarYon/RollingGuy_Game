bef6f0552ca862d2f21ce39091750eaf54593a75
Antonio Sarmiento <antoniosaryon155@gmail.com>
2024-02-08 16:32:05 -0500

    Menu implementado a medias

--- PlatformerAEL/Assets/Scripts/UI/MenuController.cs
+++ PlatformerAEL/Assets/Scripts/UI/MenuController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public void StartFadeIn()
+    {
+        GetComponent<Animator>().Play("FadeIn");
+    }
 
     public void StartGame()
     {
-
+        SceneManager.LoadScene("Level1");
     }
 
     //----------------------------------------------
